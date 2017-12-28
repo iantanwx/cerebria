5ac7dc43c38b5ecd22f578ba456a2bb3725d70ad
Ian Tan <iantanwx@gmail.com>
2017-12-28 22:21:30 +0800

    Remove unnecessary overengineering

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -2,10 +2,5 @@ import * as React from "react";
 import { Router } from "react-router-dom";
 import { Provider } from "react-redux";
 
-import { store } from "./src/store";
-
 exports.replaceRouterComponent = ({ history }) =>
-  ({ children }) =>
-    <Provider store={store} >
-      <Router history={history}>{children}</Router>
-    </Provider>;
+  ({ children }) => <Router history={history}>{children}</Router>;
