38d85cb0f2ea0e7358657b558a30912949d273b6
Ian Tan <iantanwx@gmail.com>
2017-12-29 11:18:49 +0800

    Run prettier

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,10 @@
 const path = require('path');
 const slash = require('slash');
-const {uniq, get, compact, times, reduce} = require('lodash');
+const { uniq, get, compact, times, reduce } = require('lodash');
 const uuid = require('uuid/v4');
 
-// Don't forget to update hard code values into:
-// - `templates/blog-page.tsx:23`
-// - `pages/blog.tsx:26`
-// - `pages/blog.tsx:121`
 const POSTS_PER_PAGE = 10;
-const cleanArray = arr => compact(uniq(arr));
+const cleanArray = (arr) => compact(uniq(arr));
 
 // Add Gatsby's extract-graphql Babel plugin (we'll chain it with babel-loader)
 const extractQueryPlugin = path.resolve(
@@ -18,7 +14,7 @@ const extractQueryPlugin = path.resolve(
 
 // Temporary workaround to ensure Gatsby builds minified, production build of React.
 // https://github.com/fabien0102/gatsby-starter/issues/39#issuecomment-343647558
-exports.modifyWebpackConfig = ({config, stage}) => {
+exports.modifyWebpackConfig = ({ config, stage }) => {
   if (stage === 'build-javascript') {
     config.loader('typescript', {
       test: /\.tsx?$/,
@@ -33,10 +29,8 @@ exports.modifyWebpackConfig = ({config, stage}) => {
   }
 };
 
-// Create slugs for files.
-// Slug will used for blog page path.
-exports.onCreateNode = ({node, boundActionCreators, getNode}) => {
-  const {createNodeField} = boundActionCreators;
+exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
+  const { createNodeField } = boundActionCreators;
   let slug;
   switch (node.internal.type) {
     case 'MarkdownRemark': {
@@ -44,59 +38,51 @@ exports.onCreateNode = ({node, boundActionCreators, getNode}) => {
       const [basePath, name] = fileNode.relativePath.split('/');
 
       if (basePath && name) {
-        createNodeField({node, name: 'slug', value: `/${basePath}/${name}`});
+        createNodeField({ node, name: 'slug', value: `/${basePath}/${name}` });
       }
 
-      createNodeField({node, name: 'id', value: uuid()});
+      createNodeField({ node, name: 'id', value: uuid() });
     }
 
     default:
-
   }
 };
 
-// Implement the Gatsby API `createPages`.
-// This is called after the Gatsby bootstrap is finished
-// so you have access to any information necessary to
-// programatically create pages.
-exports.createPages = ({graphql, boundActionCreators}) => {
-  const {createPage} = boundActionCreators;
+exports.createPages = ({ graphql, boundActionCreators }) => {
+  const { createPage } = boundActionCreators;
 
   return new Promise((resolve, reject) => {
     const templates = ['Post'].reduce((mem, templateName) => {
       return Object.assign({}, mem, {
-        [templateName]: path.resolve(
-          `src/templates/${templateName}.tsx`
-        )
+        [templateName]: path.resolve(`src/templates/${templateName}.tsx`)
       });
     }, {});
 
-    graphql(`
-        {
-          posts: allMarkdownRemark {
-            edges {
-              node {
-                fields {
-                  slug
-                }
-                frontmatter {
-                  tags
-                }
+    const query = graphql(`
+      {
+        posts: allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+              frontmatter {
+                tags
               }
             }
           }
         }
-      `
-    ).then(result => {
+      }
+    `).then((result) => {
       if (result.errors) {
         return reject(result.errors);
       }
-      const posts = result.data.posts.edges.map(p => p.node);
+      const posts = result.data.posts.edges.map((p) => p.node);
 
       // Create blog pages
       posts
-        .filter(post => post.fields.slug.startsWith('/blog/'))
-        .forEach(post => {
+        .filter((post) => post.fields.slug.startsWith('/blog/'))
+        .forEach((post) => {
           createPage({
             path: post.fields.slug,
             component: slash(templates.Post),
