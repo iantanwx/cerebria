d1631fe296ee1a93ba638ec185fd3ec43e4d96a9
Ian Tan <iantanwx@gmail.com>
2017-12-29 12:59:41 +0800

    Add generic styled Link component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
-import Link from 'gatsby-link';
+import Link from '../Link';
 import items from './items';
 import styled from 'styled-components';
 
 const List = styled.ul`
   list-style: none;
+  margin-left: 0;
 `;
 
 const Item = styled.li`
   display: inline-block;
+  margin: 1em;
 `;
 
 export default class Menu extends React.Component<any> {
