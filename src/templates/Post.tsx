6349bdb3ad968fba355dc730bd7ebf780fac1abe
Ian Tan <iantanwx@gmail.com>
2017-12-30 16:56:21 +0800

    Add simple post page

--- src/templates/Post.tsx
+++ src/templates/Post.tsx
@@ -1,11 +1,102 @@
 import * as React from 'react';
+import { Header1, Header2 } from '../components/Headers';
 
 export default class Post extends React.Component<any> {
   render() {
-    console.warn(this.props);
+    const { data } = this.props;
 
     return (
-      <div>A BLOG POST</div>
+      <div>
+        <Header1>{data.post.frontmatter.title}</Header1>
+        <div dangerouslySetInnerHTML={{ __html: data.post.html }} />
+      </div>
     );
   }
 }
+
+export const pageQuery = graphql`
+  query TemplateBlogPost($slug: String!) {
+    post: markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      excerpt
+      timeToRead
+      fields {
+        slug
+      }
+      frontmatter {
+        tags
+        author {
+          id
+          bio
+          twitter
+          avatar {
+            children {
+              ... on ImageSharp {
+                responsiveResolution(width: 80, height: 80, quality: 100) {
+                  src
+                  srcSet
+                }
+              }
+            }
+          }
+        }
+        title
+        updatedDate(formatString: "MMM D, YYYY")
+        image {
+          children {
+            ... on ImageSharp {
+              responsiveResolution(width: 900, height: 300, quality: 100) {
+                src
+                srcSet
+              }
+            }
+          }
+        }
+      }
+    }
+    recents: allMarkdownRemark(
+      filter: {
+        fields: { slug: { ne: $slug } }
+        frontmatter: { draft: { ne: true } }
+        fileAbsolutePath: { regex: "/blog/" }
+      }
+      sort: { order: DESC, fields: [frontmatter___updatedDate] }
+      limit: 4
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          timeToRead
+          frontmatter {
+            title
+            image {
+              children {
+                ... on ImageSharp {
+                  responsiveResolution(width: 300, height: 100) {
+                    src
+                    srcSet
+                  }
+                }
+              }
+            }
+            author {
+              id
+              avatar {
+                children {
+                  ... on ImageSharp {
+                    responsiveResolution(width: 36, height: 36) {
+                      src
+                      srcSet
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
